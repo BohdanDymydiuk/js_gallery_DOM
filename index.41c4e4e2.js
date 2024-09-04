e1f965a097e87cbdd114317a018bb90c2ffabc12
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-09-04 11:17:51 +0300

    make build

---
+++ index.41c4e4e2.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");e.src=r.href});
+//# sourceMappingURL=index.41c4e4e2.js.map
